1eeda4e99ca4b2f4f3c2c835c8435d75041ee0b3
elenamp28 <elenamp28@gmail.com>
2023-11-29 17:47:38 +0100

    API exercises

--- s7_deployment/exercise_files/exercises.py
+++ s7_deployment/exercise_files/exercises.py
@@ -1,17 +1,27 @@
 import requests
 
+def check_status(status_code):
+    if status_code == 200:
+        print('SUCCESS!')
+    elif status_code == 404:
+        print('FAILURE!')
+    else:
+        print('unknown status.')
+
 # urls = ['https://api.github.com/this-api-should-not-exist','https://api.github.com']
 
 # for url in urls:
 #     response = requests.get(url)
-#     if response.status_code == 200:
-#         print(f'{url}: SUCCESS!')
-#     elif response.status_code == 404:
-#         print(f'{url}: FAILURE!')
-#     else:
-#         print(f'{url}: unknown status.')
+#     check_status(response.status_code)
 
-response=requests.get('https://imgs.xkcd.com/comics/making_progress.png')
-resp_json = response.json()
-for k in resp_json.keys():
-    print(k)
+# response=requests.get('https://imgs.xkcd.com/comics/making_progress.png')
+# resp_json = response.json()
+# for k in resp_json.keys():
+#     print(k)
+
+# with open(r'img.png','wb') as f:
+#     f.write(response.content)
+
+pload = {'username':'Olivia','password':'123'}
+response = requests.post('https://httpbin.org/post', data = pload)
+check_status(response.status_code)
